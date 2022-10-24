21db61e43e5a868fce31ce62ac8117cbafbb1fc5
Ahmad jaberimotlagh <80390287+omidjaberim@users.noreply.github.com>
2022-10-24 20:26:58 +0330

    Update flightsInfo.ts

--- flightsInfo.ts
+++ flightsInfo.ts
@@ -4,17 +4,21 @@ interface Flight {
     minPrice: number;
 }
 const flightsInfo = (a: Flight[]) => {
-    let fId: number[] = [];
-    let fIsGuarantee:boolean[] = [];
-    let fMinPrice:number[] = [];
     let retArr:Flight[] = []
-    for (let i = 0; i <a.length ; i++) {
-        if (!fId.length) { fId[i] = a[i].flightId; fIsGuarantee[i] = a[i].isGuarantee;  fMinPrice[i] = a[i].minPrice}
-        else if(fId.includes(a[i].flightId)){
-            retArr[fId[i]] = {flightId : fId[i] , isGuarantee : a[i].isGuarantee || fIsGuarantee[fId[i]]  ,minPrice: Math.min(a[i].minPrice,fMinPrice[fId[i]])  } 
-        }
+   
+    for (let i = 0; i < a.length; i++) {
+        if (!retArr.length) {
+            retArr[a[i].flightId].flightId = a[i].flightId;
+            retArr[a[i].flightId].isGuarantee = a[i].isGuarantee;
+            retArr[a[i].flightId].minPrice = a[i].minPrice
+        } /// first loop actually
         else
-            retArr[fId[i]] = {flightId : fId[i] , isGuarantee : a[i].isGuarantee,minPrice:a[i].minPrice  } 
+            retArr[a[i].flightId] = {
+                flightId: a[i].flightId,
+                isGuarantee: a[i].isGuarantee || retArr[a[i].flightId].isGuarantee, // choose the true one
+                minPrice: Math.min(a[i].minPrice,retArr[a[i].flightId].minPrice) // choose min
+            } 
     } 
+
     return retArr
 }
